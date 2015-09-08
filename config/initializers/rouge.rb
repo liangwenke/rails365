6fec64f289bcc82623fc6264439b1f0ecd0f7ac6
suifeng2012 <ganweiliang886@gmail.com>
2015-09-08 14:23:27 +0800

    markdown在新页面打开链接

--- config/initializers/rouge.rb
+++ config/initializers/rouge.rb
@@ -8,6 +8,11 @@ module Redcarpet
       include Rouge::Plugins::Redcarpet
       # overriding Redcarpet method
       # github.com/vmg/redcarpet/blob/master/lib/redcarpet/render_man.rb#L9
+
+      def initialize(extensions = {})
+        super extensions.merge(link_attributes: { target: "_blank" })
+      end
+
       def block_code(code, language)
         # highlight some code with a given language lexer
         # and formatter: html or terminal256
